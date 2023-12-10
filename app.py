e3199c24436c026d6fb66b97f65d795f1fec84f9
Ridhwan Firdaus Nur Prasetia <86599207+oneraid@users.noreply.github.com>
2023-12-10 17:33:57 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,100 @@
+#app.py
+from flask import Flask, request, jsonify, render_template
+from food_model import FoodModel
+
+import tensorflow as tf
+import tensorflow_recommenders as tfrs
+import pandas as pd
+import numpy as np
+import re
+import random
+
+app = Flask(__name__)
+
+df_recipes = pd.read_csv('data/recipes_reduce.csv')
+df_reviews = pd.read_csv('data/reviews_reduce.csv')
+df_reviews = df_reviews.astype({'AuthorId': 'string'})
+
+Reviews = tf.data.Dataset.from_tensor_slices(dict(df_reviews[['AuthorId', 'Name', 'Rating']]))
+Recipes = tf.data.Dataset.from_tensor_slices(dict(df_recipes[['Name']]))
+
+Reviews = Reviews.map(lambda x: {
+    "Name": x["Name"],
+    "AuthorId": x["AuthorId"],
+    "Rating": float(x["Rating"])
+})
+
+Recipes = Recipes.map(lambda x: x["Name"])
+
+food_titles = Recipes.batch(1_000)
+user_ids = Reviews.batch(1_000).map(lambda x: x["AuthorId"])
+
+unique_food_titles = np.unique(np.concatenate(list(food_titles)))
+unique_user_ids = np.unique(np.concatenate(list(user_ids)))
+
+# Inisialisasi model tanpa memuat bobot
+model = FoodModel(rating_weight=1.0,
+                  retrieval_weight=1.0,
+                  unique_user_ids=unique_user_ids,
+                  unique_food_titles=unique_food_titles,
+                  Recipes=Recipes)
+
+# Bangun model dengan memanggil pada input dummy
+dummy_input = {
+    "AuthorId": [str(unique_user_ids[0])],
+    "Name": [str(unique_food_titles[0])],
+    "Rating": [0.0]  # Tidak perlu diubah ke string
+}
+
+for key in dummy_input:
+    dummy_input[key] = tf.constant(dummy_input[key], dtype=tf.string if key != "Rating" else tf.float32)
+
+# Panggil model untuk membangun struktur internal
+model(dummy_input)
+
+# Muat bobot setelah model dibangun
+model.load_weights('model/food_recommendation_model.h5')
+
+
+# Function to recommend food for a given user
+def recommend_food_for_random_user(model, recipe_df, top_n=5):
+    random_user_id = df_reviews['AuthorId'].sample(1).values[0] # Pilih secara acak dari unique_user_ids
+    index = tfrs.layers.factorized_top_k.BruteForce(model.user_model)
+    index.index_from_dataset(
+        tf.data.Dataset.zip((Recipes.batch(100), Recipes.batch(100).map(model.food_model)))
+    )
+
+    _, titles = index(tf.constant([str(random_user_id)]))
+
+    recommended_titles = [title.decode("utf-8") for title in titles[0, :top_n].numpy()]
+    return recommended_titles, random_user_id
+
+# Function to get image URL for a given recipe name
+def get_image_url(recipe_df, recipe_name, image_column='Images'):
+    image_urls = recipe_df[recipe_df['Name'] == recipe_name][image_column].values
+    if len(image_urls) > 0:
+        match = re.search(r'https://.*?\.(jpg|JPG|jpeg)', image_urls[0])
+        if match:
+            return match.group()
+    return None
+
+# Route for the home page with recommendations
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    recommendations = []
+
+    # Call the function to recommend food for a random user
+    recommended_titles, random_user_id = recommend_food_for_random_user(model, df_recipes, top_n=5)
+
+    for i, title in enumerate(recommended_titles):
+        image_url = get_image_url(df_recipes, title)
+        recommendations.append({
+            'title': title,
+            'image_url': image_url
+        })
+
+    return render_template('index.html', recommendations=recommendations, random_user_id=random_user_id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ food_model.py
@@ -0,0 +1,77 @@
+#food_model.py
+import tensorflow as tf
+import tensorflow_recommenders as tfrs
+import numpy as np
+from typing import Dict, Text
+
+class FoodModel(tfrs.models.Model):
+
+    def __init__(self, rating_weight, retrieval_weight, unique_user_ids, unique_food_titles, Recipes):
+
+        super().__init__()
+
+        embedding_dimension = 64
+
+        self.food_model: tf.keras.layers.Layer = tf.keras.Sequential([
+            tf.keras.layers.StringLookup(
+                vocabulary=unique_food_titles, mask_token=None),
+            tf.keras.layers.Embedding(len(unique_food_titles) + 1, embedding_dimension)
+        ])
+        self.user_model: tf.keras.layers.Layer = tf.keras.Sequential([
+            tf.keras.layers.StringLookup(
+                vocabulary=unique_user_ids, mask_token=None),
+            tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding_dimension)
+        ])
+
+        # We can make this as complicated as we want as long as we output a scalar
+        # as our prediction.
+        self.rating_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(256, activation="relu"),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(1),
+        ])
+
+        # The tasks.
+        self.rating_task: tf.keras.layers.Layer = tfrs.tasks.Ranking(
+            loss=tf.keras.losses.MeanSquaredError(),
+            metrics=[tf.keras.metrics.RootMeanSquaredError()],
+        )
+        self.retrieval_task: tf.keras.layers.Layer = tfrs.tasks.Retrieval(
+            metrics=tfrs.metrics.FactorizedTopK(
+                candidates=Recipes.batch(128).map(self.food_model)
+            )
+        )
+
+        # The loss weights.
+        self.rating_weight = rating_weight
+        self.retrieval_weight = retrieval_weight
+
+    def call(self, features: Dict[Text, tf.Tensor]) -> tf.Tensor:
+        user_embeddings = self.user_model(features["AuthorId"])
+        food_embeddings = self.food_model(features["Name"])
+
+        return (
+            user_embeddings,
+            food_embeddings,
+            # We apply the multi-layered rating model to a concatenation of
+            self.rating_model(
+                tf.concat([user_embeddings, food_embeddings], axis=1)
+            ),
+        )
+
+    def compute_loss(self, features: Dict[Text, tf.Tensor], training=False) -> tf.Tensor:
+
+        ratings = features.pop("Rating")
+
+        user_embeddings, food_embeddings, rating_predictions = self(features)
+
+        # We compute the loss for each task.
+        rating_loss = self.rating_task(
+            labels=ratings,
+            predictions=rating_predictions,
+        )
+        retrieval_loss = self.retrieval_task(user_embeddings, food_embeddings)
+
+        # And combine them using the loss weights.
+        return (self.rating_weight * rating_loss
+                + self.retrieval_weight * retrieval_loss)
